b0c57f7c655a8b9f43fdd8c148d20df246370d82
Lam Nguyen <lamnh@pixiostudio.com>
2019-03-19 23:57:32 +0700

    Setup MFC project

---
+++ Server/ServerDlg.h
@@ -0,0 +1,32 @@
+
+// ServerDlg.h : header file
+//
+
+#pragma once
+
+
+// CServerDlg dialog
+class CServerDlg : public CDialogEx
+{
+// Construction
+public:
+	CServerDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_SERVER_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
